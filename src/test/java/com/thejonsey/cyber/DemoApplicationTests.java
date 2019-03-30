05e191e1d91f06960cedac3554c161b9c450e44d
The-Jonsey <bobandlouis@hotmail.com>
2019-03-30 19:34:03 +0000

    Fixed tests

---
+++ src/test/java/com/thejonsey/cyber/DemoApplicationTests.java
@@ -0,0 +1,25 @@
+package com.thejonsey.cyber;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
+public class DemoApplicationTests {
+
+	@Autowired
+	private ApplicationContext applicationContext;
+
+	@Test
+	public void contextLoads() {
+		Assert.assertNotNull("App should have loaded", applicationContext);
+	}
+
+}
